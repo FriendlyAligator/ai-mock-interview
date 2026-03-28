e2764e9d17e4aed2ee94d4394baa1641704a894d
Sanskar Shaw <133490433+FriendlyAligator@users.noreply.github.com>
2026-03-28 23:55:24 +0530

    Limit password length in hashing functions
    Updated password hashing functions to limit input length to 72 characters.

--- app/auth.py
+++ app/auth.py
@@ -22,12 +22,10 @@ pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
 # Password Hashing
 def hash_password(password: str):
-    return pwd_context.hash(password)
-
+    return pwd_context.hash(password[:72])
 
 def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
+    return pwd_context.verify(plain_password[:72], hashed_password)
 
 # Create JWT Token
 def create_access_token(data: dict):

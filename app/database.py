0a61e09335be5203d01490fc90de859b59da2734
Sanskar Shaw <133490433+FriendlyAligator@users.noreply.github.com>
2026-03-28 20:24:29 +0530

    Fix database crash
    Added a check for DATABASE_URL to prevent crashes.

--- app/database.py
+++ app/database.py
@@ -3,7 +3,12 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL)
+
+# fix database crash
+if DATABASE_URL:
+    engine = create_engine(DATABASE_URL)
+else:
+    engine = None
 
 SessionLocal = sessionmaker(
     autocommit=False,

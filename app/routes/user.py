a7bff2d615d2a0bc0f685fccf908e55e02969dc9
Sanskar Shaw <133490433+FriendlyAligator@users.noreply.github.com>
2026-03-29 11:17:28 +0530

    Update user.py

--- app/routes/user.py
+++ app/routes/user.py
@@ -42,13 +42,14 @@ def register(user: schemas.UserCreate, db: Session = Depends(get_db)):
 
 # Login User
 @router.post("/login", response_model=schemas.Token)
-def login(user: schemas.UserLogin, db: Session = Depends(get_db)):
-    db_user = db.query(models.User).filter(models.User.email == user.email).first()
+def login(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+
+    db_user = db.query(models.User).filter(models.User.email == form_data.username).first()
 
     if not db_user:
         raise HTTPException(status_code=400, detail="Invalid credentials")
 
-    if not verify_password(user.password, db_user.password):
+    if not verify_password(form_data.password, db_user.password):
         raise HTTPException(status_code=400, detail="Invalid credentials")
 
     access_token = create_access_token(data={"sub": db_user.email})
